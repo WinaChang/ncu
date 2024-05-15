00219611f63b4970a39c14f38f7d84264d6b64c4
Jim <smart70094@gmail.com>
2024-05-15 23:01:00 +0800

    增加 cors 設置

--- src/main/java/com/ncu/NcuApplication.java
+++ src/main/java/com/ncu/NcuApplication.java
@@ -8,6 +8,9 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
 @Configuration
 public class NcuApplication {
@@ -18,14 +21,20 @@ public class NcuApplication {
 
     @Bean
     public CorsFilter corsFilter() {
+        List<String> allowedOrigins = Arrays.asList(
+                "http://localhost:3000",
+                "http://127.0.0.1:3000"
+        );
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         // 允許跨域請求的來源，* 表示允許所有來源
-        config.addAllowedOrigin("*");
+        config.setAllowedOrigins(allowedOrigins);
         // 允許跨域請求的方法，例如 GET、POST、PUT、DELETE 等
         config.addAllowedMethod("*");
         // 允許跨域請求包含的頭信息
         config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
